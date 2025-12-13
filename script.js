5fdb367cfe86c256eeb77ab21175483bd1185f16
Eniola <eniolaadebanjo60@gmail.com>
2025-12-13 20:09:32 +0100

    updated the javascript

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ menuOpenButton.addEventListener("click", () => {
 
 menuCloseButton.addEventListener("click", () => menuOpenButton.click
 ());
+
+const swiper = new Swiper('.slider-wrapper',{
+    loop: true,
+    spaceBetween: 25,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+         768: {
+            slidesPerView: 2
+        },
+         1024: {
+            slidesPerView: 3 
+        },
+    }
+});
