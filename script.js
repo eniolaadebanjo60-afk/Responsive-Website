ef8f32b20c2dc9925cc3ed3636346f0c34d66880
Eniola <eniolaadebanjo60@gmail.com>
2025-12-16 15:15:43 +0100

    added footer, made responsive and updated the javascript.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link")
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
@@ -7,6 +8,10 @@ menuOpenButton.addEventListener("click", () => {
 
 menuCloseButton.addEventListener("click", () => menuOpenButton.click
 ());
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click());
+})
 
 const swiper = new Swiper('.slider-wrapper',{
     loop: true,
